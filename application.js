5efdec0cf81eeec63de7583b469fb2311076e5f4
MrKaii <clarke.dwane@kippnyccp.org>
2017-06-09 11:24:10 -0400

    Create application.js

--- application.js
+++ application.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-  $("3rd").click(function(){
+  $("new_img").click(function(){
     alert("sam is trash in mancala");
   });
   $("#3rd").click(function(){
     $("#new_img").css("opacity","1");
   });
+
 });
