0c712263c962d8d6ecb23f0abba977a08661afa2
MrKaii <clarke.dwane@kippnyccp.org>
2017-06-09 11:22:21 -0400

    Create application.js

--- application.js
+++ application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $("img").click(function(){
-    alert("sam is trash in nba2k17");
+  $("3rd").click(function(){
+    alert("sam is trash in mancala");
   });
   $("#3rd").click(function(){
     $("#new_img").css("opacity","1");
